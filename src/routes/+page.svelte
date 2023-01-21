0b0b0b1069533e429453a974e9788bc6322602dc
Navindu <navanavi3@gmail.com>
2023-01-21 11:51:46 +0530

    remove tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { fly } from 'svelte/transition';
 	import { onMount } from 'svelte';
-	import { quadOut } from 'svelte/easing';
+	import { circOut } from 'svelte/easing';
 
 	let mounted = false;
 
@@ -11,19 +11,5 @@
 </script>
 
 {#if mounted}
-	<main class="w-full h-screen flex items-center justify-center">
-		<div class="w-full max-w-7xl flex">
-			<div class="w-3/4 flex flex-col gap-3">
-				<div class="h-40 relative overflow-hidden">
-					<div
-						class="absolute inset-0 text-9xl"
-						in:fly={{ y: 150, duration: 1100, easing: quadOut }}
-					>
-						Some
-					</div>
-				</div>
-			</div>
-			<div class="w-1/4 bg-blue-300" />
-		</div>
-	</main>
+	<div in:fly={{ y: 150, duration: 1100, easing: circOut }}>Text</div>
 {/if}
